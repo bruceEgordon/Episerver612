dc543824689e6e6660507d903725ff59da56cdbe
Bruce Gordon <bruce@focusls.com>
2018-09-26 14:40:09 -0400

    1

--- CommerceTraining/Infrastructure/CartAndCheckout/CustomTaxCalculator.cs
+++ CommerceTraining/Infrastructure/CartAndCheckout/CustomTaxCalculator.cs
@@ -1,113 +1,61 @@
-﻿using EPiServer.Commerce.Order.Calculator;
+﻿using EPiServer;
+using EPiServer.Commerce.Catalog.ContentTypes;
+using EPiServer.Commerce.Order;
+using EPiServer.Commerce.Order.Calculator;
+using EPiServer.Core;
+using Mediachase.Commerce;
+using Mediachase.Commerce.Catalog;
+using Mediachase.Commerce.Catalog.Managers;
+using Mediachase.Commerce.Orders;
+using Mediachase.Commerce.Orders.Managers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using EPiServer;
-using EPiServer.Commerce.Order;
-using Mediachase.Commerce.Catalog;
-using Mediachase.Commerce;
-using Mediachase.Commerce.Orders;
-using EPiServer.ServiceLocation;
-using EPiServer.Core;
-using EPiServer.Commerce.Catalog.ContentTypes;
-using Mediachase.Commerce.Catalog.Managers;
 
-namespace CommerceTraining.Infrastructure.CartAndCheckout
+namespace CommerceTraining.Infrastructure
 {
-    public class CustomTaxCalculator : DefaultTaxCalculator // changes in ECF 12
+    public class CustomTaxCalculator : ITaxCalculator
     {
-        private ReferenceConverter _referenceConverter;
-        private IContentRepository _contentRepository;
-        private IOrderGroup _orderGroup;
-        private bool peopleFromStockholm = false; // those guys will pay more tax
-        private IOrderAddress address;
+        private readonly IContentRepository _contentRepository;
+        private readonly ReferenceConverter _referenceConverter;
+        private readonly ITaxCalculator _defaultTaxCalculator;
 
-        public CustomTaxCalculator(
-            IContentRepository contentRepository
-            , ReferenceConverter referenceConverter
-            //, IShippingCalculator shippingCalculator // changes in ECF 12
-            //, ILineItemCalculator lineItemCalculator // changes in ECF 12
-            //, IReturnLineItemCalculator returnLineItemCalculator // changes in ECF 12
-            )
-            : base(contentRepository
-                  , referenceConverter
-                  //, shippingCalculator
-                  //, lineItemCalculator
-                  //, returnLineItemCalculator
-                  )
+        public CustomTaxCalculator(ITaxCalculator defaultTaxCalculator
+            , IContentRepository contentRepository
+            , ReferenceConverter referenceConverter)
         {
-            _referenceConverter = referenceConverter;
-            _contentRepository = contentRepository;
-        }
-
-        Injected<ICurrentMarket> _currMarket;
-        // Here is one place to plug in your custom stuff
-        protected override IEnumerable<ITaxValue> GetTaxValues(string taxCategory
-            , string languageCode, IOrderAddress orderAddress)
-        {
-
-            var theMarket = _currMarket.Service.GetCurrentMarket();
-
-            if (theMarket.MarketId.Value.ToString() != "sv")
-            {
-                return base.GetTaxValues(taxCategory, languageCode, orderAddress);
-            }
-            else
-            {
-                // get it from some other place
-                //return new[] { new CustomTaxValue(50,"sv","Too High",TaxType.SalesTax,) };
-                return base.GetTaxValues(taxCategory, languageCode, orderAddress);
-            }
+            this._defaultTaxCalculator = defaultTaxCalculator;
+            this._contentRepository = contentRepository;
+            this._referenceConverter = referenceConverter;
 
         }
 
-        // ECF 12
-        protected override Money CalculateSalesTax(ILineItem lineItem, IMarket market, IOrderAddress shippingAddress, Money basePrice)
+        [Obsolete("Don't use")]
+        public Money GetReturnTaxTotal(IReturnOrderForm returnOrderForm, IMarket market, Currency currency)
         {
-            return base.CalculateSalesTax(lineItem, market, shippingAddress, basePrice);
+            return _defaultTaxCalculator.GetReturnTaxTotal(returnOrderForm, market, currency);
         }
 
-        // ECF 12
-        protected override Money CalculateShippingTax(ILineItem lineItem, IMarket market, IOrderAddress shippingAddress, Money basePrice)
+        public Money GetSalesTax(ILineItem lineItem, IMarket market, IOrderAddress shippingAddress, Money basePrice)
         {
-            return base.CalculateShippingTax(lineItem, market, shippingAddress, basePrice);
-        }
+            //return _defaultTaxCalculator.GetSalesTax(lineItem, market, shippingAddress, basePrice);
+            //return new Money(1000 , "USD");
 
-        // use this one to get shipping taxes in a custom fashion
-        protected override Money CalculateShippingTaxTotal(IShipment shipment, IMarket market, Currency currency)
-        {
-            bool doCustom = true;
-
-            if (!doCustom)
-            {
-                return new Money(0, currency);
-            }
-            else
-            {
-                return base.CalculateShippingTaxTotal(shipment, market, currency);
-            }
-        }
-
-        // ...could maybe have use of this... with custom bizz-rules
-        Injected<IOrderGroupFactory> _orderGroupFactory;
-
-        // Extra tax for Stockholm
-        protected override Money CalculateTaxTotal(
-            IOrderGroup orderGroup
-            , IMarket market
-            , Currency currency)
-        {
             // may have use of this
             // could have Cart/PO-property like ... "CheckTaxStandAlone" for forking code (used with the bogus-cart)
-            this._orderGroup = orderGroup;
+            //this._orderGroup = orderGroup;
             decimal d = 0;
+            List<ITaxValue> taxValues = new List<ITaxValue>();
 
-            if (market.MarketId.Value == "sv")
+            if (market.MarketId.Value == "sv" && shippingAddress.City == "Stockholm")
             {
+
+                #region Old stuff
+
                 // need the shipment with the new stuff... else coding against the old stuff manually
-                IShipment ship = orderGroup.GetFirstShipment();
-                List<ITaxValue> taxValues = new List<ITaxValue>();
+                //IShipment ship = orderGroup.GetFirstShipment();
+
 
                 // Could have a generic "sv"-tax and... and onther higher for Stockholm (address.city)
                 // .. but then the below is not needed
@@ -115,80 +63,106 @@ namespace CommerceTraining.Infrastructure.CartAndCheckout
                 // Could have the "Stockholm Market" where taxes and prices are higher
 
                 // ...just for testing
-                if (ship.ShippingAddress == null)
-                {
-                    this.address = _orderGroupFactory.Service.CreateOrderAddress(orderGroup);
-                    address.CountryCode = "sv";
-                    address.CountryName = "sv";
+                //if (ship.ShippingAddress == null)
+                //{
+                //    this.address = _orderGroupFactory.Service.CreateOrderAddress(orderGroup);
+                //    address.CountryCode = "sv";
+                //    address.CountryName = "sv";
 
-                    // Like The Netherlands tourist accommodation tax ... differs between cities
-                    // when you set the city in CM-->Admin-->Taxes ... it gets excluded
-                    // and no tax is applied... have to find a "WorkAround"
-                    // the rest works...
-                    address.City = "Stockholm";
-                    address.Id = "DummyAddress";
-                    ship.ShippingAddress = address;
-                }
+                //    // Like The Netherlands tourist accommodation tax ... differs between cities
+                //    // when you set the city in CM-->Admin-->Taxes ... it gets excluded
+                //    // and no tax is applied... have to find a "WorkAround"
+                //    // the rest works...
+                //    address.City = "Stockholm";
+                //    address.Id = "DummyAddress";
+                //    ship.ShippingAddress = address;
+                //}
 
-                if (ship.ShippingAddress.City == "Stockholm")
-                {
-                    // 
-                    this.peopleFromStockholm = true;
-                }
+                //if (ship.ShippingAddress.City == "Stockholm")
+                //{
+                //    // 
+                //    this.peopleFromStockholm = true;
+                //}
 
                 // Extra tax ...
-                if (ship.ShippingAddress.City == "Stockholm")
+                //if (ship.ShippingAddress.City == "Stockholm")
+                //{
+                //foreach (var item in orderGroup.GetAllLineItems())
+                //{
+
+                #endregion
+
+                ContentReference contentLink = _referenceConverter.GetContentLink(lineItem.Code);
+
+                IPricing pricingItem = _contentRepository.Get<EntryContentBase>(contentLink) as IPricing;
+                int i = (int)pricingItem.TaxCategoryId;
+                string s = CatalogTaxManager.GetTaxCategoryNameById(i);
+
+                //var t = TaxManager.GetTaxes(new Guid(), s, market.MarketId.Value, market.MarketId.Value, null, null, null, null, null);
+                taxValues.AddRange(OrderContext.Current.GetTaxes(new Guid(), s, "sv", shippingAddress));
+                // An address have to be there if using this ... so we can match the different properties
+                //taxValues.AddRange(GetTaxValues(CatalogTaxManager.GetTaxCategoryNameById(i), "sv", shippingAddress));
+
+                foreach (var item2 in taxValues)
                 {
-                    foreach (var item in orderGroup.GetAllLineItems())
-                    {
-                        ContentReference contentLink = _referenceConverter.GetContentLink(item.Code);
-
-                        IPricing pricing = _contentRepository.Get<EntryContentBase>(contentLink) as IPricing;
-                        int i = (int)pricing.TaxCategoryId;
-
-                        // An address have to be there if using this ... so we can match the different properties
-                        taxValues.AddRange(GetTaxValues(CatalogTaxManager.GetTaxCategoryNameById(i), "sv", address));
-
-                        foreach (var item2 in taxValues)
-                        {
-                            // extra tax when shipped to Stockholm 10% more
-                            d += (decimal)(item2.Percentage + 0.10) * (item.PlacedPrice * item.Quantity);
-                        }
-                    }
-                    //var liAmount = orderGroup.Forms.Sum(x => x.GetAllLineItems().Sum(l => l.PlacedPrice * l.Quantity));
+                    // extra tax when shipped to Stockholm 10% more
+                    d += (decimal)(item2.Percentage + 0.10) * (lineItem.PlacedPrice * lineItem.Quantity);
                 }
-                else
+            }
+
+            //}
+            //var liAmount = orderGroup.Forms.Sum(x => x.GetAllLineItems().Sum(l => l.PlacedPrice * l.Quantity));
+            //}
+            else
+            {
+                //foreach (var item in orderGroup.GetAllLineItems())
+                //{
+                //ContentReference contentLink = _referenceConverter.GetContentLink(item.Code);
+
+                
+                foreach (var item2 in taxValues)
                 {
-                    foreach (var item in orderGroup.GetAllLineItems())
-                    {
-                        ContentReference contentLink = _referenceConverter.GetContentLink(item.Code);
-
-                        IPricing pricing = _contentRepository.Get<EntryContentBase>(contentLink) as IPricing;
-                        int i = (int)pricing.TaxCategoryId;
-
-                        // An address have to be there
-                        taxValues.AddRange(GetTaxValues(CatalogTaxManager.GetTaxCategoryNameById(i), "sv", address));
-
-                        foreach (var item2 in taxValues)
-                        {
-                            // not Stockholm, so no extra tax
-                            d += (decimal)(item2.Percentage) * (item.PlacedPrice * item.Quantity);
-                        }
-                    }
+                    // not Stockholm, so no extra tax
+                    d += (decimal)(item2.Percentage) * (lineItem.PlacedPrice * lineItem.Quantity);
                 }
+                //}
+            }
 
                 return new Money(d, market.DefaultCurrency) / 100;
             }
-            else
-            {
-                return base.CalculateTaxTotal(orderGroup, market, currency);
-            }
-        }
+            //else
+            //{
+            //    return _defaultTaxCalculator.GetSalesTax(lineItem, market, shippingAddress, );// (orderGroup, market, currency);
+            //}
 
-        protected override string GetTaxCategoryNameById(int taxCategoryId)
+        
+        [Obsolete("Don't use")]
+        public Money GetShippingReturnTaxTotal(IShipment shipment, IMarket market, Currency currency)
         {
-            return base.GetTaxCategoryNameById(taxCategoryId);
+            return _defaultTaxCalculator.GetShippingReturnTaxTotal(shipment, market, currency);
         }
 
+        public Money GetShippingTax(ILineItem lineItem, IMarket market, IOrderAddress shippingAddress, Money basePrice)
+        {
+            return _defaultTaxCalculator.GetShippingTax(lineItem, market, shippingAddress, basePrice);
+        }
+
+        [Obsolete("Don't use")]
+        public Money GetShippingTaxTotal(IShipment shipment, IMarket market, Currency currency)
+        {
+            return _defaultTaxCalculator.GetShippingTaxTotal(shipment, market, currency);
+        }
+
+        [Obsolete("Don't use")]
+        public Money GetTaxTotal(IOrderGroup orderGroup, IMarket market, Currency currency)
+        {
+            return _defaultTaxCalculator.GetTaxTotal(orderGroup, market, currency);
+        }
+
+        [Obsolete("Don't use")]
+        public Money GetTaxTotal(IOrderForm orderForm, IMarket market, Currency currency)
+        {
+            return _defaultTaxCalculator.GetTaxTotal(orderForm, market, currency);
+        }
     }
 }
