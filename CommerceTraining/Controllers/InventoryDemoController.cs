3365db272e6bfdb64527f9b327edd251b22b4ab8
Bruce Gordon <bruce@focusls.com>
2018-11-02 15:39:35 -0400

    1

--- CommerceTraining/Controllers/InventoryDemoController.cs
+++ CommerceTraining/Controllers/InventoryDemoController.cs
@@ -1,7 +1,9 @@
 ﻿using CommerceTraining.Models.Catalog;
 using CommerceTraining.Models.ViewModels;
 using EPiServer;
+using EPiServer.Commerce.Catalog;
 using Mediachase.Commerce.Catalog;
+using Mediachase.Commerce.InventoryService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,11 +16,16 @@ namespace CommerceTraining.Controllers
     {
         private ReferenceConverter _referenceConverter;
         private IContentLoader _contentLoader;
+        private AssetUrlResolver _assetUrlResolver;
+        private IInventoryService _inventoryService;
 
-        public InventoryDemoController(ReferenceConverter referenceConverter, IContentLoader contentLoader)
+        public InventoryDemoController(ReferenceConverter referenceConverter, IContentLoader contentLoader,
+            AssetUrlResolver assetUrlResolver, IInventoryService inventoryService)
         {
             _referenceConverter = referenceConverter;
             _contentLoader = contentLoader;
+            _assetUrlResolver = assetUrlResolver;
+            _inventoryService = inventoryService;
         }
         // GET: InventoryDemo
         public ActionResult Index()
@@ -26,6 +33,9 @@ namespace CommerceTraining.Controllers
             var viewModel = new InventoryDemoViewModel();
             var shirtRef = _referenceConverter.GetContentLink("Long Sleeve Shirt White Small_1");
             viewModel.Shirt = _contentLoader.Get<ShirtVariation>(shirtRef);
+            viewModel.ImageUrl = _assetUrlResolver.GetAssetUrl(viewModel.Shirt);
+
+            viewModel.Inventories = _inventoryService.QueryByEntry(new[] { viewModel.Shirt.Code });
 
             return View(viewModel);
         }
--- CommerceTraining/Models/ViewModels/InventoryDemoViewModel.cs
+++ CommerceTraining/Models/ViewModels/InventoryDemoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommerceTraining.Models.Catalog;
+using Mediachase.Commerce.InventoryService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,7 @@ namespace CommerceTraining.Models.ViewModels
     public class InventoryDemoViewModel
     {
         public ShirtVariation Shirt { get; set; }
+        public string ImageUrl { get; set; }
+        public IEnumerable<InventoryRecord> Inventories { get; set; }
     }
 }
