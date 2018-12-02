3e9943e8487e9c8eb41a30cf9392f0e49cea91d5
Bruce Gordon <bruce@focusls.com>
2018-12-02 11:16:04 -0500

    1

--- CustomBfHandler/DemoPipelineHandler.cs
+++ CustomBfHandler/DemoPipelineHandler.cs
@@ -1,6 +1,8 @@
-﻿using Mediachase.BusinessFoundation.Data.Business;
+﻿using Mediachase.BusinessFoundation.Data;
+using Mediachase.BusinessFoundation.Data.Business;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +11,13 @@ namespace CustomBfHandler
 {
     public class DemoPipelineHandler : BaseRequestHandler, IPlugin
     {
+        private StringBuilder sb = new StringBuilder();
         public new void Execute(BusinessContext context)
         {
             switch (context.PluginStage)
             {
                 case EventPipeLineStage.All:
                     break;
-
                 case EventPipeLineStage.MainOperation:
                     break;
                 case EventPipeLineStage.None:
@@ -24,7 +26,6 @@ namespace CustomBfHandler
                     PostExecWork(context);
                     break;
                 case EventPipeLineStage.PostMainOperationInsideTranasaction:
-                    PostTransactionalWork(context);
                     break;
                 case EventPipeLineStage.PreMainOperation:
                     PreExecWork(context);
@@ -39,17 +40,39 @@ namespace CustomBfHandler
 
         private void PreExecWork(BusinessContext context)
         {
-            throw new NotImplementedException();
-        }
+            sb.AppendLine($"-- Pre Exec {DateTime.Now.ToLongTimeString()}--");
 
-        private void PostTransactionalWork(BusinessContext context)
-        {
-            throw new NotImplementedException();
+            var whatClass = context.GetTargetMetaClassName();
+            sb.AppendLine($"The meta class is: {whatClass}");
+            sb.AppendLine($"The method called: {context.GetMethod().ToString()}");
+
+            PrimaryKeyId pk = (PrimaryKeyId)context.GetTargetPrimaryKeyId();
+            EntityObject entity = BusinessManager.Load(whatClass, pk);
+            sb.AppendLine($"The club card Title: {entity["TitleField"]}");
+            sb.AppendLine("bruce was here");
+            WriteValuesToLog();
         }
 
         private void PostExecWork(BusinessContext context)
         {
-            throw new NotImplementedException();
+            sb.AppendLine($"-- Post Exec {DateTime.Now.ToLongTimeString()}--");
+
+            var whatClass = context.GetTargetMetaClassName();
+            sb.AppendLine($"The meta class is: {whatClass}");
+            sb.AppendLine($"The method called: {context.GetMethod().ToString()}");
+
+            PrimaryKeyId pk = (PrimaryKeyId)context.GetTargetPrimaryKeyId();
+            EntityObject entity = BusinessManager.Load(whatClass, pk);
+            sb.AppendLine($"The club card Title: {entity["TitleField"]}");
+
+            WriteValuesToLog();
+        }
+        private void WriteValuesToLog()
+        {
+            using (StreamWriter outfile = new StreamWriter(@"C:\Episerver612\_CustomLogs\MyBfHandlerFile.txt", true))
+            {
+                outfile.Write(sb.ToString());
+            }
         }
     }
 }
